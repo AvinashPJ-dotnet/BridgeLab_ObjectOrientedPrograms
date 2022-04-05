728cecf957661a4c13e37e568dd317826f5c2448
AvinashPJ-dotnet <avinash.pujari42@gmail.com>
2022-04-05 20:16:30 +0530

    Added: StockPortfolio and Stock pojo class

---
+++ src/Stock.java
@@ -0,0 +1,38 @@
+public class Stock {
+    private String stokeName;
+    private int noOfShare;
+    private double sharePrice;
+    public double totalStockValue;
+
+    public double getTotalStockValue() {
+        return totalStockValue;
+    }
+
+    public void setTotalStockValue(double totalStockValue) {
+        this.totalStockValue = totalStockValue;
+    }
+
+    public String getStokeName() {
+        return stokeName;
+    }
+
+    public void setStokeName(String stokeName) {
+        this.stokeName = stokeName;
+    }
+
+    public int getNoOfShare() {
+        return noOfShare;
+    }
+
+    public void setNoOfShare(int noOfShare) {
+        this.noOfShare = noOfShare;
+    }
+
+    public double getSharePrice() {
+        return sharePrice;
+    }
+
+    public void setSharePrice(double sharePrice) {
+        this.sharePrice = sharePrice;
+    }
+}
