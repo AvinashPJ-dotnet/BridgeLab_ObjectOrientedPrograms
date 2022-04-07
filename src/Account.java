864413d95a1f9eca442a4de05fd1a61cb0622e18
AvinashPJ-dotnet <avinash.pujari42@gmail.com>
2022-04-07 14:27:24 +0530

    Added: buy sell function and refactored code

---
+++ src/Account.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Account {
+
+    static ArrayList<List> stockTransactionArray = new ArrayList<>();
+    static ArrayList<List> stockStorage = new ArrayList<>();
+    static double totalAmount;
+    static double balance;
+}
